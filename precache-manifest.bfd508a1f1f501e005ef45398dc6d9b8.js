94578e6341ff6757df26dfbb869454cad2f9a6bc
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2023-02-22 15:22:40 +0530

    Updates

---
+++ precache-manifest.bfd508a1f1f501e005ef45398dc6d9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad5eefbfb0500dd4791b5e76618a393",
+    "url": "/speech-recognizer-gpt/index.html"
+  },
+  {
+    "revision": "99ebed6320d6d0aef1b5",
+    "url": "/speech-recognizer-gpt/static/css/main.5b6e777f.chunk.css"
+  },
+  {
+    "revision": "bd8678e413c069ae9c9e",
+    "url": "/speech-recognizer-gpt/static/js/2.a1ddf7ca.chunk.js"
+  },
+  {
+    "revision": "9fd8ac69f8779f68b807eb06f9423c10",
+    "url": "/speech-recognizer-gpt/static/js/2.a1ddf7ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ebed6320d6d0aef1b5",
+    "url": "/speech-recognizer-gpt/static/js/main.089a98d7.chunk.js"
+  },
+  {
+    "revision": "5fe81ac025f78af5d80b",
+    "url": "/speech-recognizer-gpt/static/js/runtime-main.f6c84b2e.js"
+  }
+]);
