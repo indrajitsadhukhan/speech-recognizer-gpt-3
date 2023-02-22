2103d462b7e096a4cf53f0ac5de2c2ea0761d515
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2023-02-22 15:08:07 +0530

    first

---
+++ backend/async.js
@@ -0,0 +1,114 @@
+import request from "request";
+import cheerio from "cheerio";
+import fs from "fs";
+import React from 'react'
+
+import textversionjs from "textversionjs";
+import axios from 'axios'
+
+var url = "https://www.pwc.in/research-and-insights-hub/immersive-outlook/immersive-outlook-with-tata-steel.html";
+var question = "What initiatives Tata steel has taken to manage carbon footprint ?"
+const max_chars=10000
+const headers = 1000
+async function askQuestion(plainText,question)
+{
+  // Call Backend OpenAI Apis for completion 
+  var responseAns="";
+  await axios.post('http://localhost:8080/completion',{
+    prompt:plainText+question
+  })
+  .then(async (response)=>{
+    if(response.status==200){
+      const parsedData = await response.data.bot
+      // console.log(parsedData)
+      responseAns= parsedData;
+      }
+      else
+      responseAns= "Error: API call failed. Try again.."
+  })
+  return responseAns
+}
+
+async function handleHTML_TV(html) {
+  // Using textversionjs library
+
+  // Omit links and images from HTML page
+  var styleConfig = {
+    linkProcess: function (href, linkText) {
+      return linkText;
+    },
+    imgProcess: function (src, alt) {
+      return alt;
+    },
+  };
+
+  // Extract text from HTML page
+  var plainText = textversionjs(html, styleConfig);
+
+
+  // Write the text into a file
+  fs.writeFileSync("Output.txt", plainText, (err) => {
+    if (err) throw err;
+  });
+
+  plainText=plainText.substring(headers,headers+Math.min(max_chars,plainText.length))
+  // Ask Question from the parsed text
+  var question="\nWhat India needs to do to achieve the status of a developed economy by 2047 ?"
+  console.log("Question: " + question)
+  console.log("\nAnswer: "+ await askQuestion(plainText,question))
+}
+async function handleHTML_CR(html){
+  var plainText=""
+  // Using cheerio library
+  const select = cheerio.load(html)
+  const selectors=["p","h1","h2","h3","h4","li","sup"]
+  for(let k=0;k<selectors.length;k++)
+  {
+  let nums=select(selectors[k])
+  for(let i=0;i<nums.length;i++)
+  {
+   plainText+= select(nums[i]).text()
+  //    console.log(data)
+
+      }
+  }
+    // Write the text into a file
+    fs.writeFile("Output.txt", plainText, (err) => {
+      if (err) throw err;
+    });
+  plainText=plainText.substring(headers,headers+Math.min(max_chars,plainText.length))
+  // Ask Question from the parsed text
+  question="Write a detailed answer after understanding the above passage. "+question
+  console.log("Question: "+ question)
+  console.log("\nAnswer: "+ await askQuestion(plainText,"\n" + question))
+}
+request(url, async function (error, response, html) {
+  if (error) {
+    console.error("error3:", error);
+  } else {
+    await handleHTML_CR(html);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ backend/server.js
@@ -0,0 +1,168 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+dotenv.config()
+// This apis are created following https://platform.openai.com/docs/api-reference/edits/create?lang=node.js
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Access OpenAI APIs at localhost:8080/ using POST request
+
+// Create completion
+app.post('/completion', async (req, res) => {
+  try {
+    // Prompt = Content + Instruction
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 300, 
+      top_p: 1, 
+      frequency_penalty: 0.5,
+      presence_penalty: 0, 
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    // console.error(error)
+    console.log("Error 2")
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+// Create edit (Example - Spelling mistake, Change tone of text)
+app.post('/edits', async (req, res) => {
+  try {
+    const input = req.body.input
+    const instruction = req.body.instruction
+
+    const response = await openai.createEdit({
+      model: "text-davinci-edit-001",
+      input: `${input}`,
+      instruction: `${instruction}`,
+      temperature:0.2
+      
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+// Create image
+app.post('/images/generations', async (req, res) => {
+  try {
+
+    const prompt = req.body.prompt
+    const size = req.body.size
+    const number = req.body.number
+    const response = await openai.createImage({
+      prompt: prompt,
+      n:  number,
+      size: size
+    });
+""
+    res.status(200).send({
+      bot: response.data
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+// Create image edit
+app.post('/images/edits', async (req, res) => {
+  try {
+
+    const image = req.body.image
+    const mask = req.body.mask
+    const prompt=req.body.prompt
+    const number=req.body.number
+    const size=req.body.size
+
+    const response = await openai.createEdit(
+      image,
+      mask,
+      prompt,
+      number,
+      size
+    );
+
+    res.status(200).send({
+      bot: response.data
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+// Create image variation
+app.post('/images/variations', async (req, res) => {
+  try {
+
+    const image = req.body.image
+    const number = req.body.number
+    const size=req.body.size
+
+
+    const response = await openai.createEdit(
+      image,
+      number,
+      size
+    );
+
+    res.status(200).send({
+      bot: response.data
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+// 
+
+// POST Request to upload a new file
+app.post("/upload", (req, res) => {
+    // use modules such as express-fileupload, Multer, Busboy
+    // TODO: API to be implemented
+
+    setTimeout(() => {
+        console.log('file uploaded')
+        return res.status(200).json({ result: true, msg: 'file uploaded' });
+    }, 3000);
+});
+
+// DELETE Request to delete an existing file
+app.delete("/upload", (req, res) => {
+    // TODO: API to be implemented
+    
+    console.log(`File deleted`)
+    return res.status(200).json({ result: true, msg: 'file deleted' });
+});
+
+app.listen(8080, () => {
+    console.log(`Server running on port 8080`)
+});
